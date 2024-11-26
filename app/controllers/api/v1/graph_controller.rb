1011ca681cb4cfe1d1064e5cbac670b023d91509
Jeremie <jeremie.olivier.pro@gmail.com>
2024-11-26 14:53:18 +0100

    map Response to i7n GraphQL one

--- app/controllers/api/v1/graph_controller.rb
+++ app/controllers/api/v1/graph_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::GraphController < ApplicationController
   def index
+    render_graph_data
+  end
+
+  def create
+    render_graph_data
+  end
+
+  private
+
+  def render_graph_data
     atoms = Atom.all.map do |atom|
       {
         id: atom.label,
@@ -11,16 +21,20 @@ class Api::V1::GraphController < ApplicationController
     triples = Triple.all.map do |triple|
       {
         id: triple.id,
+        label: triple.id,
         subject: {
           id: triple.subject.label,
+          label: triple.subject.label,
           type: triple.subject_type
         },
         predicate: {
           id: triple.predicate.label,
+          label: triple.predicate.label,
           type: triple.predicate_type
         },
         object: {
           id: triple.object.label,
+          label: triple.object.label,
           type: triple.object_type
         },
         creator: triple.creator.label
@@ -28,8 +42,10 @@ class Api::V1::GraphController < ApplicationController
     end
 
     render json: {
-      atoms: atoms,
-      triples: triples
+      data: {
+        triples: triples
+      }
+
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :graph, only: [ :index ]
+      resources :graph, only: [ :index, :create ]
     end
   end
 end
