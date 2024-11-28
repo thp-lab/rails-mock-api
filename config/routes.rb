fa5de2f8b4bb38d13af49e1eabd19e12f97fde8c
Maximelodato <maxime.lodato@gmail.com>
2024-11-28 17:29:35 +0100

    creation d'une vue pour cree des triplets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # HTML view route
+  root 'graph#index'
+  resources :graph, only: [ :index, :create ]
+  
+  # API routes
   namespace :api do
     namespace :v1 do
       resources :graph, only: [ :index, :create ]
