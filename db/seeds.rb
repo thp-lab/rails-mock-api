9081b2f407b3ede99ad94131031fbe6501a1e085
Maximelodato <maxime.lodato@gmail.com>
2024-11-28 11:31:29 +0100

    modif seed triple

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 
 
-# Load seed files in order of dependency
-# 1. Load base concepts and shared atoms first
-require_relative 'seeds/students'
+# # Load seed files in order of dependency
+# # 1. Load base concepts and shared atoms first
+# require_relative 'seeds/students'
 
-# 2. Load skill ontology that builds on base concepts
-require_relative 'seeds/skills'
+# # 2. Load skill ontology that builds on base concepts
+# require_relative 'seeds/skills'
 
-# 3. Load session data that uses concepts from both
-require_relative 'seeds/thp_sessions'
+# # 3. Load session data that uses concepts from both
+# require_relative 'seeds/thp_sessions'
+
+require_relative 'seeds/create'
 
 puts "Seeding completed!"
