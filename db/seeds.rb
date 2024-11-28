50e456e0edcffcdbd149eb961b88299342d9902c
Maximelodato <maxime.lodato@gmail.com>
2024-11-28 12:39:44 +0100

    maj seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,4 @@
-
-
-# # Load seed files in order of dependency
-# # 1. Load base concepts and shared atoms first
-# require_relative 'seeds/students'
-
-# # 2. Load skill ontology that builds on base concepts
-# require_relative 'seeds/skills'
-
-# # 3. Load session data that uses concepts from both
-# require_relative 'seeds/thp_sessions'
-
-require_relative 'seeds/create'
+# Load seed files in order of dependency
+require_relative 'seeds/real_data'
 
 puts "Seeding completed!"
