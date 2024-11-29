65855183faf09c7a7705102fbbf34c90b3162239
Maximelodato <maxime.lodato@gmail.com>
2024-11-29 08:22:51 +0100

    vue charger tous les Atoms (de type Triples ou Atoms)  Lorsque nous tapons dans un champ, le résultat est filtré pour n’afficher que des résultats similaires  Un bouton ** Créer un triplet ** enregistre les nouveaux triplets créés dans la base de données  Les triplets nouvellement créés apparaîssent dans la liste des atomes disponibles

--- app/controllers/api/v1/graph_controller.rb
+++ app/controllers/api/v1/graph_controller.rb
@@ -4,16 +4,65 @@ class Api::V1::GraphController < Api::BaseController
   end
 
   def create
-    render_graph_data
+    begin
+      creator = Creator.find_or_create_by!(
+        label: 'The Hacking Project',
+        image: 'thp_logo.png'
+      )
+
+      subject = Atom.find_or_create_by!(
+        label: triple_params[:subject_label],
+        creator_label: creator.label,
+        creator: creator
+      )
+
+      predicate = Atom.find_or_create_by!(
+        label: triple_params[:predicate_label],
+        creator_label: creator.label,
+        creator: creator
+      )
+
+      object = Atom.find_or_create_by!(
+        label: triple_params[:object_label],
+        creator_label: creator.label,
+        creator: creator
+      )
+
+      triple = Triple.find_or_create_by!(
+        subject: subject,
+        predicate: predicate,
+        object: object,
+        creator: creator
+      )
+
+      render_graph_data
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: "Erreur de validation: #{e.message}" }, status: :unprocessable_entity
+    rescue StandardError => e
+      render json: { error: "Une erreur est survenue: #{e.message}" }, status: :internal_server_error
+    end
   end
 
   private
 
+  def triple_params
+    params.require(:triple).permit(:subject_label, :predicate_label, :object_label)
+  end
+
   def render_graph_data
+    atoms = Atom.all.map do |atom|
+      {
+        id: atom.label,
+        label: atom.label,
+        type: 'Atom'
+      }
+    end
+
     triples = Triple.all.map do |triple|
       {
         id: triple.id,
         label: triple.id,
+        type: 'Triple',
         subject: {
           id: triple.subject.label,
           label: triple.subject.label,
@@ -35,7 +84,7 @@ class Api::V1::GraphController < Api::BaseController
 
     render json: {
       data: {
-        triples: triples
+        atoms: atoms + triples
       }
     }
   end
