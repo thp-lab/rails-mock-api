efb5557e7409fd7332a6d4b2cc97e2a04189cfa2
Maximelodato <maxime.lodato@gmail.com>
2024-11-29 11:59:40 +0100

    remise en fonction de l'affichage de l'api graph v1

--- app/controllers/api/v1/graph_controller.rb
+++ app/controllers/api/v1/graph_controller.rb
@@ -4,6 +4,46 @@ class Api::V1::GraphController < Api::BaseController
   end
 
   def create
+    if request.content_type == 'application/json' && request.raw_post.include?('query')
+      # Gestion de la requête GraphQL
+      handle_graphql_request
+    else
+      # Gestion de la requête REST standard
+      handle_rest_request
+    end
+  end
+
+  private
+
+  def handle_graphql_request
+    query = JSON.parse(request.raw_post)['query']
+    if query.include?('triples')
+      triples_data = Triple.includes(:subject, :predicate, :object).limit(500).map do |triple|
+        {
+          id: triple.id,
+          subject: {
+            label: triple.subject.label,
+            id: triple.subject.label # Utiliser le label comme ID pour les atomes
+          },
+          predicate: {
+            label: triple.predicate.label,
+            id: triple.predicate.label
+          },
+          object: {
+            label: triple.object.label,
+            id: triple.object.label
+          }
+        }
+      end
+      render json: { data: { triples: triples_data } }
+    else
+      render json: { error: "Requête GraphQL non supportée" }, status: :unprocessable_entity
+    end
+  rescue JSON::ParserError
+    render json: { error: "Format de requête invalide" }, status: :bad_request
+  end
+
+  def handle_rest_request
     begin
       creator = Creator.find_or_create_by!(
         label: 'The Hacking Project',
@@ -43,17 +83,15 @@ class Api::V1::GraphController < Api::BaseController
     end
   end
 
-  private
-
   def triple_params
     params.require(:triple).permit(:subject_label, :predicate_label, :object_label)
   end
 
   def render_graph_data
-    triples = Triple.all.map do |triple|
+    triples = Triple.includes(:subject, :predicate, :object, :creator).map do |triple|
       {
         id: triple.id,
-        label: triple.id,
+        label: triple.label,
         type: 'Triple',
         subject: {
           id: triple.subject.label,
@@ -74,7 +112,7 @@ class Api::V1::GraphController < Api::BaseController
       }
     end
 
-    atoms = Atom.all.map do |atom|
+    atoms = Atom.includes(:creator).map do |atom|
       {
         id: atom.label,
         label: atom.label,
